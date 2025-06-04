b2280f10aad963505f55383bc37d6fa803977e63
Huỳnh Thanh Tâm <55596752+TamHuynhTee@users.noreply.github.com>
2025-06-04 17:28:18 +0700

    feat: Use npm on semantic release

--- release.config.js
+++ release.config.js
@@ -3,15 +3,9 @@ const config = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/git",
     "@semantic-release/github",
+    "@semantic-release/npm",
   ],
 };
 
