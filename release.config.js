e9cba6da069b061044e83decd4b45b908cea59dc
Huỳnh Thanh Tâm <55596752+TamHuynhTee@users.noreply.github.com>
2025-06-09 23:40:52 +0700

    fix: bypass the semantic git version bump for now

--- release.config.js
+++ release.config.js
@@ -35,14 +35,14 @@ module.exports = {
         successCmd: "scripts/generate-sri.sh ${nextRelease.version}",
       },
     ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "package-lock.json"], // or just "package.json"
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["package.json", "package-lock.json"], // or just "package.json"
+    //     message:
+    //       "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    //   },
+    // ],
     [
       "@semantic-release/github",
       {
