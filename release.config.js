b7bd70c6050a2061c8a5e725ed3f02d44e643aa6
Huỳnh Thanh Tâm <55596752+TamHuynhTee@users.noreply.github.com>
2025-06-09 23:17:46 +0700

    feat: @semantic-release/git for auto package.json version bump

--- release.config.js
+++ release.config.js
@@ -36,6 +36,14 @@ module.exports = {
       },
     ],
     [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json"], // or just "package.json"
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
       "@semantic-release/github",
       {
         assets: ["dist/**/*"],
